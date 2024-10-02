a1ae877ae19ce4df6506d8d7c497419118f6ac44
Amanda Ruiz <98482837+amilpsp@users.noreply.github.com>
2024-10-02 15:39:57 +0200

    set base for gh-pages deploy

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,11 @@ import { PrimeVueResolver } from "@primevue/auto-import-resolver";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [
     vue(),
     Components({
-      resolvers: [
-        PrimeVueResolver()
-      ]
-    })
-  ]
-})
+      resolvers: [PrimeVueResolver()],
+    }),
+  ],
+});
